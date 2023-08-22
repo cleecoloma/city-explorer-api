28fac4785da9e0b66d57b1ba82373f612be30e95
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-22 11:35:13 -0700

    added nodemon

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ const cors = require('cors');
 dotenv.config();
 const PORT = process.env.PORT;
 
-const server = express(); //create our express server, now we are ready to define some functionality.
-server.use(cors()); //activates cross-origin-resource-sharing
+const app = express(); //create our express app, now we are ready to define some functionality.
+app.use(cors()); //activates cross-origin-resource-sharing. It will allow other origins (besides localhost to make request to this code)
 
-server.get('/weather', (request, response) => {
-  console.log('We got the weather report!')
-  response.send('Hello World!')
+app.get('/weather', (request, response) => {
+  console.log(request.query);
+  console.log('We got the weather report!');
+
+  if (!request.query.search) {
+    response.status(400).send('Bad Request');
+  } else {
+    response.status(200).send('Hello World!');
+  }
 });
-// server.post()
-// server.put()
+// app.post()
+// app.put()
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('Server is listening!');
-})
+});
