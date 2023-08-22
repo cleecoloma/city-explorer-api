306f1d5280b3548306e5c614c7c4bcfa2baa07e6
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-22 11:22:23 -0700

    added dotenv and cors methods

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const dotenv = require('dotenv');
 const express = require('express'); //built in function for code running in the Node runtime.
+const cors = require('cors');
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 const server = express(); //create our express server, now we are ready to define some functionality.
+server.use(cors()); //activates cross-origin-resource-sharing
 
 server.get('/weather', (request, response) => {
   console.log('We got the weather report!')
@@ -11,6 +17,6 @@ server.get('/weather', (request, response) => {
 // server.post()
 // server.put()
 
-server.listen(3001, () => {
+server.listen(PORT, () => {
   console.log('Server is listening!');
 })
