cda6832fe17ea3fdbccb6942c81d045f309dc1ef
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-23 22:19:16 -0700

    movie and weather info now displays on the front end

--- index.js
+++ index.js
@@ -16,7 +16,19 @@ class Forecast {
   }
 }
 
-const dataRetrieve = (response) => {
+class Movies {
+  constructor(title, overview, averageVotes, totalVotes, imageUrl, popularity, releasedOn) {
+    this.title = title;
+    this.overview = overview;
+    this.averageVotes = averageVotes;
+    this.totalVotes = totalVotes;
+    this.imageUrl = imageUrl;
+    this.popularity = popularity;
+    this.releasedOn = releasedOn;
+  }
+}
+
+const weatherDataRetrieve = (response) => {
   const cityWeatherData = [];
   for (let i = 0; i < 5; i++) {
     const forecast = new Forecast(
@@ -26,6 +38,23 @@ const dataRetrieve = (response) => {
     cityWeatherData.push(forecast);
   }
   return cityWeatherData;
+};
+
+const movieDataRetrieve = (response) => {
+  const cityMovieData = [];
+  for (let i = 0; i < 20; i++) {
+    const movies = new Movies(
+      response.results[i].title,
+      response.results[i].overview,
+      response.results[i].vote_average,
+      response.results[i].vote_count,
+      response.results[i].poster_path,
+      response.results[i].popularity,
+      response.results[i].release_date
+    );
+    cityMovieData.push(movies);
+  }
+  return cityMovieData;
 };
 
 const errorMessage = (errorCode) => {
@@ -47,30 +76,33 @@ const errorMessage = (errorCode) => {
 const app = express(); //create our express app, now we are ready to define some functionality.
 app.use(cors()); //activates cross-origin-resource-sharing. It will allow other origins (besides localhost to make request to this code)
 
-app.get('/weather', (request, response) => {
-  const { lat, lon } = request.query;
+app.get('/weather', async (request, response) => {
+  const { lat, lon, searchQuery } = request.query;
   console.log('We got the weather report!');
   console.log(request.query);
   debugger;
-  if (!lat || !lon) {
+  if (!lat || !lon || !searchQuery) {
     console.log('here now');
     errorMessage(400);
   } else {
     console.log('Located before weatherbit get request');
-    axios
-      .get(
+    try {
+      let weatherResponse = await axios.get(
         `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lon=${lon}&lat=${lat}`
-      )
-      .then((weatherResponse) => {
-        // console.log('WeatherBit - Successful: ', response.data);
-        let sendWeatherDataToClient = dataRetrieve(weatherResponse.data);
-        console.log(sendWeatherDataToClient);
-        response.send(sendWeatherDataToClient);
-      })
-      .catch((error) => {
-        console.log(`Error: didn't load get request`, error);
-        response.status(500).send('Something went wrong!');
-      });
+      );
+      let sendWeatherDataToClient = weatherDataRetrieve(weatherResponse.data);
+      // console.log(sendWeatherDataToClient);
+      // response.send(sendWeatherDataToClient);
+      let movieResponse = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${searchQuery}`
+      );
+      let sendMovieDataToClient = movieDataRetrieve(movieResponse.data);
+      // console.log(sendMovieDataToClient);
+      response.send({sendWeatherDataToClient, sendMovieDataToClient});
+    } catch (error) {
+      console.log(`Error: didn't load get request`, error);
+      response.status(500).send('Something went wrong!');
+    }
   }
 });
 
